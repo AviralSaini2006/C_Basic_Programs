53484ee81c3747964e2296156634a0a0d542fb04
Aviral Saini <aviralsaini1509@gmail.com>
2025-11-02 12:42:42 +0530

    Version 1

--- factorial.c
+++ factorial.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+int fac(int S)
+{
+    int c, F = 1;
+    for (c = 1; c <= S; c++)
+    {
+        F = F * c;
+    }
+    return F;
+}
 int main()
 {
-    int n, S = 0, F = 1, c;
+    int n;
     printf("Enter the number : ");
     scanf("%d", &n);
 
-    for (c = 1; c <= n; c++)
-    {
-        F = F * c;
-        S = S + F;
-    }
-    printf("The result is %d\n", S);
+    printf("The result is %d\n", fac(n));
 }
